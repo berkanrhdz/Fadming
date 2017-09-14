05a73e1bbe94c2e9983c9b18fdf537895627ab4c
eduescobar13 <eduescal13@gmail.com>
2017-09-15 00:05:25 +0100

    Solucionado el problema de mostrar los usuarios en el perfil particular.

--- Web/huertos.php
+++ Web/huertos.php
@@ -32,20 +32,31 @@
                 <div class="contenedor-contenido">
                     <div class="contenedor-informacion-pagina">
                       <div class="contenedor-huertos-usuarios">
-                        <div class="contenedor-usuarios">
-                          <div class="gestion-usuarios-huerto">
-                            <div id="titulo-usuarios-huerto">PERMISOS DE USUARIOS</div>
-                            <div id="lista-usuarios-huerto">
-                              <div id="nombre-huerto-permisos"></div>
-                              <div class="contenedor-usuarios-permisos">
-                                <div id="mensaje-ayuda-permisos"><b>Pulse sobre el nombre de un huerto.</b></br>
-                                                                    Luego, elija entre los usuarios de la empresa, aquellos con <b>permiso para escanear los códigos QR</b> del huerto seleccionado</div>
-                                <div id="usuarios-permisos"></div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                        <div class="contenedor-huertos">
+                          <?php if($_SESSION['tipo'] == 1) {
+                                  echo "<div class='contenedor-mensaje-huerto-particular'>";
+                                  echo "<div class='mensaje-huerto-particular'>";
+                                  echo "<div id='mensaje'><b>Gestione sus huertos usando el cuadro de la izquierda</b>Cambia de finca usando las flechas de la parte superior, luego añade todo los huertos necesarios";
+                                  echo "</div></div></div>";
+                                }
+                                else if ($_SESSION['tipo'] == 2) {
+                                  echo "<div class='contenedor-usuarios'>";
+                                  echo "<div class='gestion-usuarios-huerto'>";
+                                  echo "<div id='titulo-usuarios-huerto'>PERMISOS DE USUARIOS</div>";
+                                  echo "<div id='lista-usuarios-huerto'>";
+                                  echo "<div id='nombre-huerto-permisos'></div>";
+                                  echo "<div class='contenedor-usuarios-permisos'>";
+                                  echo "<div id='mensaje-ayuda-permisos'><b>Pulse sobre el nombre de un huerto.</b></br>Luego, elija entre los usuarios de la empresa, aquellos con <b>permiso para escanear los códigos QR</b> del huerto seleccionado</div>";
+                                  echo "<div id='usuarios-permisos'></div>";
+                                  echo "</div></div></div></div>";
+                                }
+                          ?>
+                          <?php if($_SESSION['tipo'] == 1) {
+                                  echo "<div class='contenedor-huertos' style='width: 65%; float: left;'>";
+                                }
+                                else if ($_SESSION['tipo'] == 2) {
+                                  echo "<div class='contenedor-huertos' style='width: 60%; float: right;'>";
+                                }
+                          ?>
                           <div class="gestion-huertos">
                             <div class="titulo-finca" id="titulo-finca">
                               <div id="flecha-izquierda"></div>
